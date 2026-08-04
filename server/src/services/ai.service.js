f49ba763add6b34ae3b9de619c34d1b1c3152a66
smabdurrehmanshah <smabdurrahmanshah@gmail.com>
2026-08-04 14:11:22 +0500

    Fixing puppeteer bug

--- server/src/services/ai.service.js
+++ server/src/services/ai.service.js
@@ -131,14 +131,12 @@ const generateInterviewReport = async ({
 const generatePdfFromHtml = async (htmlContent) => {
   try {
     const browser = await puppeteer.launch({
-      executablePath: await puppeteer.executablePath(),
       headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
 
-    console.log("Executable Path: ", browser.executablePath);
-    console.log("Executable Path: ", executablePath);
+    console.log("Executable Path: ", puppeteer.executablePath);
 
     const page = await browser.newPage();
 
